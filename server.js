5412ed0b473ccf864b51401f687b8d2dd473b913
Pat Lowe <lowe-patrick@att.net>
2023-07-10 17:08:10 -0700

    added some text to food page

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
